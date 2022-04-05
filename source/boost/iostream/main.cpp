0960dabfcb5355395783938b13ec64de3647ebed
uuiid <957714080@qq.com>
2022-04-05 15:36:14 +0800

    完成基本示例

---
+++ source/boost/iostream/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TD on 2022/4/5.
+//
+
+#include <boost/iostreams/filter/zlib.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])  {
+    std::cout << "ok" << argc << argv << std::endl;
+}
