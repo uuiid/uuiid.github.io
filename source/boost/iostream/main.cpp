25f503fd0580ee2c3fd41d696ab094def23c55aa
uuiid <957714080@qq.com>
2022-04-05 16:17:00 +0800

    完成boost zlip示例

--- source/boost/iostream/main.cpp
+++ source/boost/iostream/main.cpp
@@ -2,9 +2,25 @@
 // Created by TD on 2022/4/5.
 //
 
-#include <boost/iostreams/filter/zlib.hpp>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
 
-int main(int argc, char *argv[])  {
-    std::cout << "ok" << argc << argv << std::endl;
+#include <fmt/format.h>
+
+int main(int argc, char *argv[]) {
+    std::stringstream istringstream_source{"sanduiosahfpdsjfsdnfbgvfzx0-i["
+                                           "ksz["
+                                           "fjpisf"};
+    std::stringstream istringstream_target{};
+    boost::iostreams::filtering_streambuf<boost::iostreams::input> filteringStreambuf{};
+    filteringStreambuf.push(boost::iostreams::zlib_compressor{});
+    filteringStreambuf.push(istringstream_source);
+    boost::iostreams::copy(filteringStreambuf, istringstream_target);
+
+    std::cout << fmt::format("str |{}| -> |{:#x}|", istringstream_source.str(), fmt::join(istringstream_target.str()," "));
+
 }
