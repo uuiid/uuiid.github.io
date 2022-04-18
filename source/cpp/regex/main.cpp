dc70ada8d494dae9fe3077b0a57824a115d7dbd7
uuiid <957714080@qq.com>
2022-04-18 18:27:02 +0800

    添加中文修正

--- source/cpp/regex/main.cpp
+++ source/cpp/regex/main.cpp
@@ -9,18 +9,22 @@
 
 int main(int argc, char *argv[]) {
   if (argc == 3) {
-    std::string l_reg{argv[1]};
-    std::string l_str{argv[2]};
-    std::regex regex{l_reg};
-    std::smatch smatch{};
-    if (std::regex_match(l_str,
-                         smatch,
-                         regex)) {
-      for (auto &&i : smatch) {
-        std::cout << i.str() << std::endl;
+    try {
+      std::wstring l_reg{boost::locale::conv::utf_to_utf<wchar_t>(argv[1])};
+      std::wstring l_str{boost::locale::conv::utf_to_utf<wchar_t>(argv[2])};
+      std::wregex regex{l_reg};
+      std::wsmatch smatch{};
+      if (std::regex_match(l_str,
+                           smatch,
+                           regex)) {
+        for (auto &&i : smatch) {
+          std::cout << boost::locale::conv::utf_to_utf<char>(i.str()) << std::endl;
+        }
+      } else {
+        std::cout << "not match " << boost::locale::conv::utf_to_utf<char>(l_str) << std::endl;
       }
-    } else {
-      std::cout << "not match " << l_str << std::endl;
+    } catch (const std::runtime_error &error) {
+      std::cout << error.what() << std::endl;
     }
     return 0;
   }
