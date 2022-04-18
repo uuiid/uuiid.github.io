4ee2cf2f550ded74aab04ea4c0da0ef1e9cdd047
uuiid <957714080@qq.com>
2022-04-18 17:10:12 +0800

    新的格式化

--- source/cpp/regex/main.cpp
+++ source/cpp/regex/main.cpp
@@ -5,24 +5,24 @@
 #include <regex>
 #include <string>
 #include <iostream>
-
+#include <boost/locale.hpp>
 
 int main(int argc, char *argv[]) {
-    if (argc == 3) {
-        std::string l_reg{argv[1]};
-        std::string l_str{argv[2]};
-        std::regex regex{l_reg};
-        std::smatch smatch{};
-        if (std::regex_match(l_str,
-                             smatch,
-                             regex)) {
-            for (auto &&i: smatch) {
-                std::cout << i.str() << std::endl;
-            }
-        } else {
-            std::cout << "not match" << std::endl;
-        }
-        return 0;
+  if (argc == 3) {
+    std::string l_reg{argv[1]};
+    std::string l_str{argv[2]};
+    std::regex regex{l_reg};
+    std::smatch smatch{};
+    if (std::regex_match(l_str,
+                         smatch,
+                         regex)) {
+      for (auto &&i : smatch) {
+        std::cout << i.str() << std::endl;
+      }
+    } else {
+      std::cout << "not match " << l_str << std::endl;
     }
-    return 1;
+    return 0;
+  }
+  return 1;
 }
