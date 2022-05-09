7d58971e15df7cae1649465371ccabc0f7f9e88a
张玉斌 <46096697+uuiid@users.noreply.github.com>
2022-05-09 09:27:46 +0800

    boost asio json rpc 示例 宏改进

--- source/boost/asio/main_server.cpp
+++ source/boost/asio/main_server.cpp
@@ -30,58 +30,67 @@ class rpc_server_name : public rpc_server {
       std::cout << "call test_4" << std::endl;
       return 1;
     });
+    //    register_fun_t("t"s, &rpc_server_name::test_bind);
+    register_fun_t("test_5", std::bind(&rpc_server_name::test_bind, this, std::placeholders::_1));
   }
+
+  virtual std::string test_bind(const std::string& in_str) = 0;
 };
 
-#define DOODLE_RPC_FUN_ARGS(args_type,args_name) args_type,args_name
+#define DOODLE_RPC_GET_FUN_NAME_STR(list) BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, list))
+#define DOODLE_RPC_GET_FUN_NAME(list) BOOST_PP_TUPLE_ELEM(1, list)
+#define DOODLE_RPC_GET_FUN_RETURN(list) BOOST_PP_TUPLE_ELEM(0, list)
+#define DOODLE_RPC_TO_ARG_DEF(elem) BOOST_PP_TUPLE_ELEM(2, elem) arg
+#define DOODLE_RPC_TO_ARG(list) BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(list), 3), DOODLE_RPC_TO_ARG_DEF(list), BOOST_PP_EMPTY())
+#define DOODLE_RPC_TO_ARG2(list) BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(list), 3), arg, BOOST_PP_EMPTY())
+#define DOODLE_RPC_FUN_DECLARATION(r, data, elem) \
+  virtual DOODLE_RPC_GET_FUN_RETURN(elem) DOODLE_RPC_GET_FUN_NAME(elem)(DOODLE_RPC_TO_ARG(elem)) = 0;
 
+#define DOODLE_RPC_CALL_FUN(r, data, elem)                                               \
+  register_fun_t(DOODLE_RPC_GET_FUN_NAME_STR(elem),                                      \
+                 [this](DOODLE_RPC_TO_ARG(elem)) -> DOODLE_RPC_GET_FUN_RETURN(elem) {        \
+                   return this->DOODLE_RPC_GET_FUN_NAME(elem)(DOODLE_RPC_TO_ARG2(elem)); \
+                 });
 
-#define DOODLE_RPC_FUN_ARGS_VAR_MAP(r, data, i, elem) data(elem)
+#define DOODLE_RPC_DECLARATION(rpc_server_name, ...)                                            \
+  class rpc_server_name : public rpc_server {                                                   \
+   public:                                                                                      \
+    using rpc_server::rpc_server;                                                               \
+    void init_register() override {                                                             \
+      BOOST_PP_SEQ_FOR_EACH(DOODLE_RPC_CALL_FUN, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__));     \
+    }                                                                                           \
+    BOOST_PP_SEQ_FOR_EACH(DOODLE_RPC_FUN_DECLARATION, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)) \
+  };
 
-#define DOODLE_RPC_FUN_REGISTER(return_value, fun_name, ...)        \
-  register_fun_t(                                                   \
-      BOOST_PP_STRINGIZE(fun_name),                                 \
-                         [this](                                    \
-                            BOOST_PP_SEQ_FOR_EACH_I(DOODLE_RPC_FUN_ARGS_VAR_MAP,DOODLE_RPC_FUN_ARGS, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)) \
-                              ) -> return_value {      \
-                           return BOOST_PP_CAT(this->, fun_name)(); \
-                         });
+DOODLE_RPC_DECLARATION(rpc_test_mar,
+                       (void, test_1),
+                       (void, test_2, const std::string&),
+                       (std::string, test_3, const std::int32_t&))
 
-#define DOODLE_RPC_FUN_DECLARATION(return_value, fun_name, ...) \
-  virtual return_value fun_name(__VA_ARGS__) = 0;
+class rpc_server_name2 : public rpc_server {
+ public:
+  using rpc_server::rpc_server;
+  void init_register() override {
+    void test(){};
+  };
 
-#define DOODLE_VAR_MAP(r, data, i, elem) BOOST_PP_CAT(data, elem)
+  int main(int argc, char* argv[]) {
+    try {
+      std::uint16_t port{0};
+      if (argc == 2) {
+        port = std::atoi(argv[1]);
+      }
 
-#define DOODLE_RPC_SUB(rpc_server_name, ...)                                                                      \
-  class rpc_server_name : public rpc_server {                                                                     \
-   public:                                                                                                        \
-    using rpc_server::rpc_server;                                                                                 \
-    void init_register() override {                                                                               \
-      BOOST_PP_SEQ_FOR_EACH_I(DOODLE_VAR_MAP, DOODLE_RPC_FUN_REGISTER, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))      \
-    }                                                                                                              \
-    BOOST_PP_SEQ_FOR_EACH_I(DOODLE_VAR_MAP, DOODLE_RPC_FUN_DECLARATION, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))     \
-};
+      boost::asio::io_context io_context;
+      boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+      signals.async_wait([&](auto, auto) { io_context.stop(); });
 
-// DOODLE_RPC_SUB(rpc_test_mar, (void, test_1, ), (void, test_2, ))
-//DOODLE_RPC_SUB(rpc_test_mar, (void, test_1 ,()), (void, test_2 ,()))
+      server s(io_context, port);
 
-int main(int argc, char* argv[]) {
-  try {
-    std::uint16_t port{0};
-    if (argc == 2) {
-      port = std::atoi(argv[1]);
+      io_context.run();
+    } catch (std::exception& e) {
+      std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    boost::asio::io_context io_context;
-    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
-    signals.async_wait([&](auto, auto) { io_context.stop(); });
-
-    server s(io_context, port);
-
-    io_context.run();
-  } catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
+    return 0;
   }
-
-  return 0;
-}
---
+++ source/boost/asio/server/json_rpc_macro.h
@@ -0,0 +1,30 @@
+//
+// Created by TD on 2022/5/9.
+//
+
+#pragma once
+
+#define DOODLE_RPC_GET_FUN_NAME_STR(list) BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, list))
+#define DOODLE_RPC_GET_FUN_NAME(list) BOOST_PP_TUPLE_ELEM(1, list)
+#define DOODLE_RPC_GET_FUN_RETURN(list) BOOST_PP_TUPLE_ELEM(0, list)
+#define DOODLE_RPC_TO_ARG_DEF(elem) BOOST_PP_TUPLE_ELEM(2, elem) arg
+#define DOODLE_RPC_TO_ARG(list) BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(list), 3), DOODLE_RPC_TO_ARG_DEF(list), BOOST_PP_EMPTY())
+#define DOODLE_RPC_TO_ARG2(list) BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(list), 3), arg, BOOST_PP_EMPTY())
+#define DOODLE_RPC_FUN_DECLARATION(r, data, elem) \
+  virtual DOODLE_RPC_GET_FUN_RETURN(elem) DOODLE_RPC_GET_FUN_NAME(elem)(DOODLE_RPC_TO_ARG(elem)) = 0;
+
+#define DOODLE_RPC_CALL_FUN(r, data, elem)                                               \
+  register_fun_t(DOODLE_RPC_GET_FUN_NAME_STR(elem),                                      \
+                 [this](DOODLE_RPC_TO_ARG(elem)) -> DOODLE_RPC_GET_FUN_RETURN(elem) {    \
+                   return this->DOODLE_RPC_GET_FUN_NAME(elem)(DOODLE_RPC_TO_ARG2(elem)); \
+                 });
+
+#define DOODLE_RPC_DECLARATION(rpc_server_name, ...)                                            \
+  class rpc_server_name : public rpc_server {                                                   \
+   public:                                                                                      \
+    using rpc_server::rpc_server;                                                               \
+    void init_register() override {                                                             \
+      BOOST_PP_SEQ_FOR_EACH(DOODLE_RPC_CALL_FUN, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__));     \
+    }                                                                                           \
+    BOOST_PP_SEQ_FOR_EACH(DOODLE_RPC_FUN_DECLARATION, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)) \
+  };
