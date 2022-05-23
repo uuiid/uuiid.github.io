4262475d690f6cf30b4ce58968678b26a0de0b5b
uuiid <957714080@qq.com>
2022-05-23 16:44:26 +0800

    添加移动测试

--- source/cpp/move/main.cpp
+++ source/cpp/move/main.cpp
@@ -19,11 +19,30 @@ class test_value {
  public:
   explicit test_value(int32_t pV) : p_v(pV) {}
   virtual ~test_value() = default;
+  test_value(test_value &&in_) noexcept {
+    p_v     = in_.p_v;
+    in_.p_v = 0;
+  };
+  test_value &operator=(test_value &&in_) noexcept {
+    p_v     = in_.p_v;
+    in_.p_v = 0;
+    return *this;
+  };
+  /**
+   * @brief 删除复制函数
+   */
+  test_value(test_value &) noexcept            = delete;
+  /**
+   * @brief 删除复制函数
+   */
+  test_value &operator=(test_value &) noexcept = delete;
+
+  std::int32_t val() { return p_v; }
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   test_value value{100};
-  test_value* value_ptr{&value};
+  test_value *value_ptr{&value};
   auto l_test = std::move(value);
-  std::cout << bool(value_ptr) << std::endl;
+  std::cout << l_test.val() << std::endl;
 }
